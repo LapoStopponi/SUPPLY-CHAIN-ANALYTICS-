5f89762289718142db734a2eedbd76181251505f
Lapo Stopponi <166521228+LapoStopponi@users.noreply.github.com>
2024-08-04 10:40:05 +0200

    Update and rename profitability to profitability.sql

--- assets/sql/analysis/profitability.sql
+++ assets/sql/analysis/profitability.sql
@@ -29,14 +29,14 @@ SELECT
 FROM shipments_data
 ORDER BY year, month, shipment_mode,managed_by;
 	
--- I've create a CTE and the used it to work with differenct windows function, - I've calculated the three months moving average, 
-for example for the Truck shipment mode managed by PMO-US, for the year 2014, -> Insights: the moving avergage shows a big values range, 
+/* I've created a CTE and used it to work with different window functions, - I've calculated the three-month moving average, 
+for example for the Truck shipment mode managed by PMO-US, for the year 2014, -> Insights: the moving average shows a big values range, 
 from about 5,6 million to 18 million. There's a clear peak in the middle of the series. The series ends higher than it stars. There are significant 
-changes between consecutive periods, indicating volatility. Largest increase between 4th and 5th values. Largest decrease between 7th and 8th values. 
-Despite fluctuations, there's a general upward tren from the beginning to the end of the series. -I've calculated the cumulative_yearly_profit, 
-that displays the cumulative profitability over the year, providing a sense of how profitability builds up over time. Taking as example the year 2008,
-for Air shipment_mode managed by PMO-US, we can see that there is a significat drop from the first to the second value, but from the secon value onward, 
-the trend is upward. The growth rate seems to slow down towards the end of the series. -I've also calulated the difference fro overall average, this
+changes between consecutive periods, indicating volatility. The largest increase between the 4th and 5th values. The largest decrease between the 7th and 8th values. 
+Despite fluctuations, there's a general upward trend from the beginning to the end of the series. -I've calculated the cumulative_yearly_profit, 
+which displays the cumulative profitability over the year, providing a sense of how profitability builds up over time. Taking as an example the year 2008,
+for Air shipment_mode managed by PMO-US, we can see that there is a significant drop from the first to the second value, but from the second value onward, 
+the trend is upward. The growth rate seems to slow down towards the end of the series. -I've also calculated the difference from the overall average, this
 highlights the deviations from the overall average profitability, pinpointing months that perform better or worse than average, while all the other
 months have less profitability than the overall average.
 */
